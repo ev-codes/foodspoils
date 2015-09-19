ebc935e122fbc53f50b72947251bc6783b38f1ba
Evan McIlroy <evanmcilroy@gmail.com>
2015-09-19 19:25:20 -0400

    React setting up leaflet markets on click of map.

--- foodspoils_express/myapp/public/components.jsx
+++ foodspoils_express/myapp/public/components.jsx
@@ -2,12 +2,33 @@ var isNode = typeof module !== 'undefined' && module.exports
 , React = isNode ? require('react/addons') : window.React;
 
 var HelloMessage = React.createClass({
-  handleClick: function () {
-      alert('You clicked!');
-  },
-  componentDidMount: function(){
-    var map = L.map('map').setView([51.505, -0.09], 13);
+    handleClick: function (e) {
+	console.log("in handle click");
+	console.log(e);
+	var pins_state = [];
+	var long = e.latlng.lat;
+	var lat = e.latlng.lat;
+	pins_state = this.state.pins;
+	console.log("pins state"+ pins_state);
+	var object ={lat: lat,
+		     long: long};
+	pins_state.push(object);
 
+	
+	this.setState({pins: pins_state});
+  },
+    componentDidMount: function(){
+		var map  = L.map('map').setView([45.5017, -73.5673], 13);
+	this.setState({map: map});
+	console.log("helloworld");
+	var that = this;
+	map.on('click', function(e) {
+	     that.handleClick(e);
+	    }
+);
+
+
+	
     L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
       maxZoom: 18,
       attribution: 'Map data © <a href="http://openstreetmap.org">OpenStreetMap</a> contributors'
@@ -15,14 +36,17 @@ var HelloMessage = React.createClass({
     console.log("hello");
   },
   getInitialState: function() {
-    return {lattitude: 45.5017,
-      longitude: 73.5673};
+      return {
+	  lattitude: 45.5017,
+	  longitude: 73.5673,
+	  pins: []
+	   };
     },
     render: function() {
       return <div onClick={this.handleClick}>
       Hello {this.props.name}
       <div className="container-fluid" id="map_container">
-      <div id="map"></div>
+	    <div id="map"></div>
       </div>
       </div>
     }
